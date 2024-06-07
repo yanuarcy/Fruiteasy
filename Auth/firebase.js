62e66567e235beb08807bbd11e2fff57218bc527
RoseD'Arc <90492011+AdiyatmaAdmaja@users.noreply.github.com>
2024-06-07 09:56:45 +0700

    Changing firebase configuration with firestore on google cloud as platform

--- Auth/firebase.js
+++ Auth/firebase.js
@@ -1,23 +1,5 @@
-const admin = require('firebase-admin');
-const firebase = require('firebase/app');
-require('firebase/auth');
+const { Firestore } = require('@google-cloud/firestore');
 
-const serviceAccount = require('./test-capstone-ce7f9-firebase-adminsdk-q5zv2-53cf45d6cb.json');
+const db = new Firestore();
 
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-});
-
-firebase.initializeApp({
-    apiKey: "AIzaSyDlSzHpMgLCUtPcRCOfySrZAtsiy7_A-D4",
-    authDomain: "test-capstone-ce7f9.firebaseapp.com",
-    projectId: "test-capstone-ce7f9",
-    storageBucket: "test-capstone-ce7f9.appspot.com",
-    messagingSenderId: "752231842091",
-    appId: "1:752231842091:web:5653cf33542b5bd12b856e",
-    measurementId: "G-GMJS2YBNQ0"
-});
-
-const db = admin.firestore();
-
-module.exports = { admin, db, firebase };
+module.exports = db;
